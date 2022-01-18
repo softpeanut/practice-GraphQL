57d5d89296a9427d1e3e25f331abddb6028dea2e
김범진 <sdpthf@naver.com>
2022-01-18 12:52:35 +0900

    ♻ :: 모듈 구조

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,19 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import 'dotenv/config';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ProductModule } from './product/product.module';
-import { Product } from './product/entity/product.entity';
+import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
+import { GraphQLConfigModule } from './graphql/graphql-config.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: 'schema.gpl',
-      include: [ProductModule]
-    }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Product],
-      synchronize: true,
-    }),
+    GraphQLConfigModule,
+    TypeOrmConfigModule,
     ProductModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {
-  constructor(private readonly connection: Connection) {}
+  constructor(private readonly connection: Connection) { }
 }
